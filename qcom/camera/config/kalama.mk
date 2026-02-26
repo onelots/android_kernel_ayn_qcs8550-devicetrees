f1a055b59729b84327c611f58f20d0282abdd6a8
Balázs Triszka <info@balika011.hu>
2026-02-26 01:34:24 -0800

    update to qcm8550-la-2-0-r00023.1

--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -4,7 +4,12 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
 								kalama-camera-sensor-hdk.dtbo \
 								kalama-sg-hhg-camera.dtbo \
-								kalama-sg-hhg-camera-sensor.dtbo
+								kalama-sg-hhg-camera-sensor.dtbo \
+								kalama-camera-sensor-aim300.dtbo \
+								kalama-camera-sensor-rb5gen2-dvt.dtbo \
+								kalama-camera-sensor-rb5gen2-evt.dtbo \
+								kalama-camera-sensor-rb5gen2-vc.dtbo \
+								kalama-camera-sensor-iot-vc.dtbo
 
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
